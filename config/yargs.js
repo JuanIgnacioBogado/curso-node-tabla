35c4116f092a7455db6e98487aa2ec0f75c275e2
Juan Ignacio Bogado <juanignacio.bogado@redtecnologica.org>
2022-02-13 03:23:08 -0300

    first commit

---
+++ config/yargs.js
@@ -0,0 +1,33 @@
+require('colors');
+
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+        describe: 'Es la base de la tabla de multiplicar'
+    })
+    .option('h', {
+        alias: 'hasta',
+        type: 'number',
+        default: 10,
+        describe: 'Es el limite hasta donde llega la multiplicacion'
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        default: false,
+        describe: 'Muestra la tabla en la consola'
+    })
+    .check((argv, options) => {
+        if(isNaN(argv.b)) {
+            throw 'La base tiene que ser un numero';
+        }
+        if(isNaN(argv.h)) {
+            throw 'El limite hasta tiene que ser un numero';
+        }
+        return true;
+    })
+    .argv;
+
+module.exports = argv;
